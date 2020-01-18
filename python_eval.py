da6b2b5b1e7c39d0f48ea7dc5d6d5fb4fa67b4de
Chandrapurnima <cpb.95.2012@gmail.com>
2020-01-18 18:35:42 +0530

    evalution function

---
+++ python_eval.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/python-eval/problem?h_r=next-challenge&h_v=zen
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+if __name__ == '__main__':
+    x = str(input())
+
+    eval(x)
+
+
